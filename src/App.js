6dcf616505caaaead2016698b2a000f3b5d37937
code-250 <richmunye@gmail.com>
2021-01-02 17:01:52 +0200

    added a function to delete todo

--- src/App.js
+++ src/App.js
@@ -10,13 +10,23 @@ class App extends Component {
 
     ]
   }
+  // function delete
+  deleteTodo = (id )=>{
+    const todos= this.state.todos.filter(todo=>{
+      return todo.id !==id
+    });
+// setting up a new array of todos
+    this.setState({
+      todos
+    })
+  }
   render(){
     return (
     <div className="App">
       <h1 className='header'>
         Todo's
       </h1>
-      <Todos todos={this.state.todos}/>
+      <Todos todos={this.state.todos} deleteTodo={this.deleteTodo}/>
     </div>
   );
 }
